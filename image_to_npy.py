419ed3238187a85fee5139706ba23923da8b8236
JackieWuuuuuu <107164806+JackieWuuuuuu@users.noreply.github.com>
2024-12-24 09:46:04 +0800

    Update image_to_npy.py

--- image_to_npy.py
+++ image_to_npy.py
@@ -6,7 +6,7 @@ from PIL import Image
 def images_to_npy(image_folder, output_npy_file):
     images = []
     for filename in os.listdir(image_folder):
-        if filename.endswith('.jpg') or filename.endswith('.png'):  # 根据需要修改文件扩展名
+        if filename.endswith('.jpg') or filename.endswith('.png'):  # 修改文件扩展名
             img_path = os.path.join(image_folder, filename)
             img = Image.open(img_path).convert('RGB')  # 将图片转换为RGB格式
             img_array = np.array(img)
@@ -20,6 +20,6 @@ def images_to_npy(image_folder, output_npy_file):
 
 
 # 使用示例
-image_folder = r'C:\UltraLight-VMUNet-Output\1_mild_npdr\1_mild_npdr_256\1_mild_npdr_changename\1_mild_npdr_original'  # 替换为你的图片文件夹路径
-output_npy_file = r'C:\UltraLight-VMUNet-Output\1_mild_npdr\1_mild_npdr_256\1_mild_npdr_changename\1_mild_npdr_original\output_images.npy'  # 替换为你想要保存的.np文件名
+image_folder = r'C:\xxx\xxx\xxx'  # 替换图片文件夹路径
+output_npy_file = r'C:\xxx\xxx\xxx.npy'
 images_to_npy(image_folder, output_npy_file)
